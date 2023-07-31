57f31e9eda3e4ad7d2188dc0a9ed26e0c8afd4d7
Diana Sosnovskaya <progdidi@gmail.com>
2023-07-31 16:16:10 +0200

    change audio format

--- app/js/main.js
+++ app/js/main.js
@@ -1,70 +1,71 @@
 $(function(){
 
-    //scroll animation
-    new WOW().init();
+  //scroll animation
+  new WOW().init();
 
-    //SCROLL AUDIO
-    const audio = new Audio();
-    audio.preload = 'auto';
-    audio.src = '../helpers/print.mp3';
-    let timer = null;
+  //SCROLL AUDIO
+  const audio = new Audio();
+  audio.preload = 'auto';
+  audio.src = '../helpers/print.mp3';
+  audio.type = 'audio/mpeg';
+  let timer = null;
 
-    document.addEventListener('scroll', () => {
-        audio.play();
+  document.addEventListener('scroll', () => {
+      audio.play();
 
-        clearTimeout(timer);
-        timer = setTimeout(function() {
-            audio.pause()
-        }, 100);
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+          audio.pause()
+      }, 100);
+  })
+
+  //HAMBURGER MENU
+  const menuBtn = document.querySelector('.menu__btn');
+  const menu = document.querySelector('.menu__list');
+
+  menuBtn.addEventListener('click', () => {
+      menu.classList.toggle('active')
+  })
+
+
+  //FIXED MENU
+  window.addEventListener('scroll', () => {
+      const ScrollHeight = window.pageYOffset;
+      const headerWrapper = document.querySelector('.header__top');
+      const headerTopHeight = headerWrapper.getBoundingClientRect().height;
+  
+      if(ScrollHeight > headerTopHeight) {
+        headerWrapper.classList.add('header__fixed');
+      } else {
+        headerWrapper.classList.remove('header__fixed');
+      }
     })
 
-    //HAMBURGER MENU
-    const menuBtn = document.querySelector('.menu__btn');
-    const menu = document.querySelector('.menu__list');
+  //CONTACTS FORM
+  const form = document.querySelector('.contacts__form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+    });
+    return false;
 
-    menuBtn.addEventListener('click', () => {
-        menu.classList.toggle('active')
-    })
-
-
-    //FIXED MENU
-    window.addEventListener('scroll', () => {
-        const ScrollHeight = window.pageYOffset;
-        const headerWrapper = document.querySelector('.header__inner');
-        const headerTopHeight = headerWrapper.getBoundingClientRect().height;
-    
-        if(ScrollHeight > headerTopHeight) {
-          headerWrapper.classList.add('header__fixed');
-        } else {
-          headerWrapper.classList.remove('header__fixed');
-        }
-      })
-
-    //CONTACTS FORM
-    const form = document.querySelector('.contacts__form');
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      $.ajax({
-        type: "POST",
-        url: "mailer/mailer/smart.php",
-        data: $(this).serialize()
-      }).done(function() {
-        $(this).find("input").val("");
-      });
-      return false;
-
-    })
+  })
 
 
-    //fixed scroll
-    const clippy = document.querySelector('.clip');
-    const step = document.querySelector('.step');
+  //fixed scroll
+  const clippy = document.querySelector('.clip');
+  const step = document.querySelector('.step');
 
-    function scrollToItem(item) {
-      clippy.addEventListener('click', () => {item.scrollIntoView();})
-    };
+  function scrollToItem(item) {
+    clippy.addEventListener('click', () => {item.scrollIntoView();})
+  };
 
-    scrollToItem(step);
+  scrollToItem(step);
 
-   
+ 
 });
